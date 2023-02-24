e4d1f8de00ccbc23bfcbbf829e0c1df55d60ae3d
Eleanor Kahn <eleanor.c.kahn@gmail.com>
2023-02-24 14:31:30 -0800

    Week 4 Assignment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function() {
     $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
+    $("#carouselButton").on("click", (function(){
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
@@ -10,5 +10,17 @@ $(function() {
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
         }
+    }));
+});
+
+$(function() {
+    $("#reserveButton").on("click", function(){
+        $("#reserveModal").modal("toggle");
+    });
+});
+
+$(function() {
+    $("#loginButton").on("click", function(){
+        $("#loginModal").modal("toggle");
     });
 });
